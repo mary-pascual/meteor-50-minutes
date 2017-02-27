35c66b0351d3e4df41e95961bbfda04dae17c449
Mary <mekp@hawaii.edu>
2017-02-26 17:21:40 -1000

    Task 4: Tasklist authentication

--- app/tasklist.js
+++ app/tasklist.js
@@ -10,16 +10,13 @@ if (Meteor.isClient) {
     "submit .add-task": function(event){
       var name = event.target.name.value;
 
-      Tasks.insert({
-        name: name,
-        createdAt: new Date()
-      });
+      Meteor.call('addTask', name);
       event.target.name.value = '';
       return false;
     },
     "click .delete-task": function(event){
       if(confirm('Delete Task?')){
-        Tasks.remove(this._id);
+        Meteor.call('deleteTask', this._id);
       }
       return false;
     }
@@ -29,3 +26,19 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
 
 }
+
+Meteor.methods({
+  add-task: function(name){
+    if(!Meteor.userId()) {
+      throw new Meteor.Error('No Access!');
+    }
+    Tasks.insert({
+      name: name,
+      createdAt: new Date(),
+      userId: Meteor.userId()
+   });
+  }
+  delete-task: function(taskId){
+    Tasks.remove(taskId);
+  }
+});
