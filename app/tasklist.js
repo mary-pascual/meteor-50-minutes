0835ab963b55fac7403cb1fad0914591701823b0
Mary <mekp@hawaii.edu>
2017-02-26 18:12:44 -1000

    Task 5: subscriptions

--- app/tasklist.js
+++ app/tasklist.js
@@ -1,6 +1,7 @@
 Tasks = new Mongo.Collection('tasks');
 
 if (Meteor.isClient) {
+  Meteor.subscribe('tasks');
   Template.tasks.helpers({
     tasks: function() {
       return Tasks.find({}, {sort: {createdAt: -1}});
@@ -25,7 +26,9 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
-
+  Meteor.publish('tasks', function(){
+    return Taks.find({userId: this.userId});
+  });
 }
 
 Meteor.methods({
