904771ec346b350bfce418c0d478023d599f25a9
Mary <mekp@hawaii.edu>
2017-02-26 17:27:24 -1000

    Task 4: Tasklist authentication

--- app/tasklist.js
+++ app/tasklist.js
@@ -6,21 +6,22 @@ if (Meteor.isClient) {
       return Tasks.find({}, {sort: {createdAt: -1}});
     }
   });
-  Template.tasks.events({
-    "submit .add-task": function(event){
-      var name = event.target.name.value;
 
+  Template.tasks.events({
+    "submit .add-task": function(event) {
+      let name = event.target.name.value;
       Meteor.call('addTask', name);
       event.target.name.value = '';
       return false;
     },
-    "click .delete-task": function(event){
-      if(confirm('Delete Task?')){
+
+    "click .delete-task": function(event) {
+      if (confirm('Delete Task?')) {
         Meteor.call('deleteTask', this._id);
       }
       return false;
     }
-  });
+  })
 }
 
 if (Meteor.isServer) {
@@ -28,17 +29,16 @@ if (Meteor.isServer) {
 }
 
 Meteor.methods({
-  add-task: function(name){
-    if(!Meteor.userId()) {
-      throw new Meteor.Error('No Access!');
+  addTask: function(name){
+    if (!Meteor.userId()) {
+      throw new Meteor.Error('No Access');
     }
     Tasks.insert({
-      name: name,
-      createdAt: new Date(),
-      userId: Meteor.userId()
-   });
-  }
-  delete-task: function(taskId){
+      name: name, createdAt: new Date(), userId: Meteor.userId()
+    });
+  },
+
+  deleteTask: function(taskId) {
     Tasks.remove(taskId);
   }
-});
+})
